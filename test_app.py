b56b6aba294305bbfe6716772c555618a97d3fbe
Shravani Budharam <shravanibudharam25@gmail.com>
2026-08-16 21:19:24 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-from app import add, subtract
+from app import app
 
-def test_add():
-    assert add(5, 5) == 10
 
-def test_subtract():
-    assert subtract(10, 5) == 5
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Welcome to Python Web Application!"
